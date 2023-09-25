e47ae217d16bbc6818caa1676df819f2e787d5e8
Mohamed10 <99076774@mydavinci.nl>
2023-09-25 11:15:49 +0200

    G

--- module_1/deel_1/de Speelhal-dag.py
+++ module_1/deel_1/de Speelhal-dag.py
@@ -1,23 +1,23 @@
 # Stel: je gaat met 3 vrienden (dus met zijn vieren) een dag naar de speelhal: ‘de Speelhal-dag’
 
-# Dat kost een TOEGANTICKET/ per persoon van 7,45 euro voor de hele dag. Jullie willen ook met zijn allen voor 45 minuten in de VIP-VR-GameSeat. De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten. Jij trakteert dus betaal je alles.
+# Dat kost een toegangsticket per persoon van 7,45 euro voor de hele dag. Jullie willen ook met zijn allen voor 45 minuten in de VIP-VR-GameSeat. De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten. Jij trakteert dus betaal je alles.
 
 # Maak een programma speelhal.py voor deze berekening.
 totaal_personen = 4
-TOEGANTICKET  = 7.45
-vip_vr_45 = 0.37
+toegangsticket  = 7.45
+vip_vr_5min = 0.37
 
-TOEGANTICKET_prijs = totaal_personen * TOEGANTICKET
+toegangsticket_prijs = totaal_personen * toegangsticket
 
-vp_prijs = (vip_vr_45 * 9) * 4
+vp_prijs = (vip_vr_5min * 9) * 4
 
-totaal = vp_prijs + TOEGANTICKET_prijs
+totaal = vp_prijs + toegangsticket_prijs
 
-roud_totaal = round(totaal)
+roud_totaal = round(totaal,2)
 
 print(f"het totaal bedrag voor {totaal_personen} personen is  {roud_totaal} euro ")
 
-print(f"Dit geweldige dagje-uit met {totaal_personen} mensen in speelhal 45 minuten VR kost je maar {roud_totaal}")
+print(f"Dit geweldige dagje-uit met {totaal_personen} mensen in speelhal 45 minuten VR kost je maar { roud_totaal}")
 
 
 
