1853fabf95bd8523d604b1861cf4a3fc181de234
Mohamed10 <99076774@mydavinci.nl>
2023-09-25 18:43:17 +0200

    sa

--- module_1/deel_3/pf.py
+++ module_1/deel_3/pf.py
@@ -1,5 +1,27 @@
-a = int(input("voer een getal in : "))
-b = int(input("voer een getal in : "))
+a = float(input("Voer de getal  a in: "))
+b = float(input("Voer de getal  b in: "))
+
 if a > b:
-    print("get a is het grootste")
+    groot = a
+    klein = b
+    print(f"a is het grootste getal {a}")
+    print(f"groot getal was {groot}")
+    print(f"klein getal was {klein}")
+
+elif a < b:
+    klein = a
+    groot = b
+    print(f"a is het kleinste {b} ")
+    print(f"groot getal was {groot}")
+    print(f"klein getal was {klein}")
+    
+
+else:
+    print(" beide zijn gelijk ")
+
+
+
+
+
+
 
