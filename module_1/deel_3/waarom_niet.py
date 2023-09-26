778a867975e3ecdc40b574553849903f4a820776
Mohamed10 <99076774@mydavinci.nl>
2023-09-26 20:40:40 +0200

    Update waarom_niet.py

--- module_1/deel_3/waarom_niet.py
+++ module_1/deel_3/waarom_niet.py
@@ -51,8 +51,6 @@ elif geslacht == "vrouw":
 else:
     glimlach_breedte = int(input("Hoe breed is uw glimlach (in cm)? : "))
 
-
-
 if rijbewijs is "ja" and MIN_GEWICHT <= gewicht <= MAX_GEWICHT and MIN_LENGTE <= lengte <= MAX_LENGTE and (dieren_dressuur >= DIEREN_ERVARING_NODIG 
 or jogleren_1 >= JOGLEREN_2ERVARING
  or  acrobatiek >= ACROBATIEK_2ERVARING) and (ondernemer_jaren >= MIN_ONDERNEMER_JAREN 
@@ -61,4 +59,10 @@ or glimlach_breedte >= MIN_GLIMLACH_BREEDTE) :
     print("goed")
 else:
     print("no")  
-    
+if rijbewijs is "nee" and MIN_GEWICHT > gewicht > MAX_GEWICHT and MIN_LENGTE > lengte > MAX_LENGTE:
+    print("je hebt proplemen met basic")
+if dieren_dressuur < DIEREN_ERVARING_NODIG or jogleren_1 < JOGLEREN_2ERVARING:
+    print("je hebt niet genoeg ervaring")
+if snorlengte < MIN_SNOR_BREEDTE or haarlengte < MIN_HAAR_LENGTE or glimlach_breedte < MIN_GLIMLACH_BREEDTE:
+    print("je snor/glimlach/roodhaar niet goes")
+
