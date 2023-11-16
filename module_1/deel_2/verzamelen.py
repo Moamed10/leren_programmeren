439ac74bd87c02eaca302af24f5448e2c385b718
Mohamed10 <99076774@mydavinci.nl>
2023-11-16 13:16:58 +0100

    l

---
+++ module_1/deel_2/verzamelen.py
@@ -0,0 +1,13 @@
+text = "Aba was erg bekend met hit watereloo"
+letter = "A"
+count = 0
+
+for x in text:
+    if x.lower() == letter.lower():
+        count += 1
+
+print(count)
+
+for x in range (6):
+    print("je bent lekker bezig ")
+
--- module_1/deelt_5/deel5_2/zinnen.py
+++ module_1/deelt_5/deel5_2/zinnen.py
@@ -1,31 +1,40 @@
 import re
 
-def get_sub_sentences(text: str) -> list:
-    # Replace alle separators door een marker "|"
+def get_sub_sentences(text):
     marked_text = re.sub(r"\.|,|!|\?| en |omdat |zodat |want |wanneer |dat ", "|", text)
-    # Split de tekst op marker "|"
     sub_sentences = marked_text.split("|")
-    # Verwijder leidende en volgende spaties
-    sub_sentences = [sentence.strip() for sentence in sub_sentences if len(sentence) > 0]
-    return sub_sentences
+    return [sentence.strip().lower() for sentence in sub_sentences if len(sentence.strip()) > 0]
 
-def calculate_ego_score(sub_sentences: list) -> int:
+def get_ego_score(sub_sentences):
     ego_score = 0
     for sentence in sub_sentences:
-        sentence = sentence.lower()  # Zet de zin om naar kleine letters
-        words = sentence.split(' ')  # Split de zin in woorden
-        # Controleer of het eerste of tweede woord gelijk is aan 'ik' of 'mijn'
+        words = sentence.split(' ')
         if len(words) >= 2 and (words[0] in ('ik', 'mijn') or words[1] in ('ik', 'mijn')):
             ego_score += 1
     return ego_score
 
-# Test de functies met een voorbeeldtekst
-example_text = "Ik ben een voorbeeldtekst. Mijn ego is groot, want ik ben fantastisch. Jij en ik, wij kunnen alles doen."
-sub_sentences = get_sub_sentences(example_text)
-ego_score = calculate_ego_score(sub_sentences)
+text1 = "Ik wil graag solliciteren naar de functie van programmeur bij uw bedrijf. Ik ben de beste kandidaat voor deze functie omdat ik al jaren ervaring heb in deze branche en ik weet dat niemand anders mijn niveau van kennis en vaardigheden kan evenaren."
+sub_sentences1 = get_sub_sentences(text1)
+print("Sub-zinnen uit tekst 1:", sub_sentences1)
 
-# Print de resultaten
-print("Deelzinnen:")
-print(sub_sentences)
-print("Ego-score:")
-print(ego_score)
+text2 = "Dit is een testzin. Hier is nog een testzin. En nog een zin voor de test."
+sub_sentences2 = get_sub_sentences(text2)
+print("Sub-zinnen uit tekst 2:", sub_sentences2)
+
+ego_score1 = get_ego_score(sub_sentences1)
+print("Ego-score voor tekst 1:", ego_score1)
+
+ego_score2 = get_ego_score(sub_sentences2)
+print("Ego-score voor tekst 2:", ego_score2)
+   
+# for sentence in sub_sentences: # repeat for every sentence in a list sentences
+#     sentence = sentence.strip() # remove leading and trailing spaces
+#     sentence = sentence.lower() # convert uppercase characters to lowercase
+#     if len(sentence) > 0:
+#       words = sentence.split(' ') # split sentence into words
+#       # first words of sentence equal to ik?
+#       if len(words) >= 2 and (words[0] in ('ik','mijn') or words[1] in ('ik','mijn')):
+#         ego_score += 1
+ 
+# print(ego_score)
+
