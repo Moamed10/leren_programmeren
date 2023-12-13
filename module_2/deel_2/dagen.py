327bf50a47fcde2d589dce7e80e3bbbc210d7d72
Mohamed10 <99076774@mydavinci.nl>
2023-12-13 11:14:11 +0100

    dagen

---
+++ module_2/deel_2/dagen.py
@@ -0,0 +1,30 @@
+dagen_van_de_week = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
+
+print (f"dagen van de week : {dagen_van_de_week}")
+
+
+print("\nWerkdagen", end=" : ")
+for x in dagen_van_de_week[0:5]:
+    print( x,end = ",")
+
+print( "\n\nweekend : " , end= ":")
+for d in dagen_van_de_week[-2:]:
+    print(   d , end= ",")
+
+
+
+print("\n\nAlle dagen van de week in omgekeerde volgorde zijn:", end = " ," )
+for a in range( 6 , -1, -1):
+    print(dagen_van_de_week[a] , end = " ,")
+
+
+
+print("\n\ne werkdagen in omgekeerde volgorde zijn: ", end = " ,")
+for a in range( 4 , -1, -1):
+    print(dagen_van_de_week[a] , end = " ,")
+
+
+
+print("\n\n\ weekend omgekeerd " ,  end=" , ")
+for x in range(6 , 4 , -1 ):
+    print(dagen_van_de_week[x] ,  end=" , ")
