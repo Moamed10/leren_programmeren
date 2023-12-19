215fe086d6781c2ac501392470b967b6fb30c204
Mohamed10 <99076774@mydavinci.nl>
2023-12-19 10:32:37 +0100

    ap

--- module_2/deel_2/oefening7.py
+++ module_2/deel_2/oefening7.py
@@ -1,3 +1,20 @@
-nmr = int(input("lijst nmr  ? "))
-aantal = int(input("nmr ? "))
-print(list(range( 0 , aantal , nmr)))
+lijst = []
+
+aantallijsten = int(input("aantal lijsten : "))
+
+for x in range(1 ,aantallijsten +1  ):
+    lengte = int(input(f"hoeveel in lijst {x} : "))
+
+
+
+for x in range(aantallijsten, lengte ):
+    lijst1 = []
+    lijst1.append(x)
+    lijst.append(lijst1)
+
+
+print(lijst)
+
+
+
+    
--- module_2/deel_2/the_list_factory.py
+++ module_2/deel_2/the_list_factory.py
@@ -1,32 +1,11 @@
-# Definieert een functie genaamd genereer_lijstjes met een parameter aantal_lijstjes
-def genereer_lijstjes(aantal_lijstjes):
-    # Maakt een lege lijst genaamd lijstjes
-    lijstjes = []
+aantal_lijstjes = int(input("Hoeveel lijstjes? "))
 
-    # Start een for-loop die loopt van 1 tot aantal_lijstjes + 1
-    for i in range(1, aantal_lijstjes + 1):
-        # Vraagt de gebruiker om de lengte van het huidige lijstje in te voeren
-        lengte_lijst = int(input(f"Hoeveel in lijst {i}? "))
-        
-        # Genereert een lijst met behulp van range en voegt deze toe aan de lijstjes
-        lijst = list(range(i, i * lengte_lijst + 1, i))
-        lijstjes.append(lijst)
+lijstjes = []
 
-    # Geeft de lijst van lijstjes terug
-    return lijstjes
-
-# Definieert de main-functie, het hoofdgedeelte van het programma
-def main():
-    # Vraagt de gebruiker om het aantal lijstjes in te voeren
-    aantal_lijstjes = int(input("Hoeveel lijstjes? "))
+for x in range(1, aantal_lijstjes + 1):
+    lengte = int(input(f"Hoeveel in lijst {x}? "))
     
-    # Roept de genereer_lijstjes-functie aan met het ingevoerde aantal lijstjes
-    resultaat = genereer_lijstjes(aantal_lijstjes)
-    
-    # Drukt het resulterende lijstje van lijstjes af
-    print(resultaat)
+    lijst1 = list(range(x, x * lengte + 1, x))
+    lijstjes.append(lijst1)
 
-# Controleert of het script rechtstreeks wordt uitgevoerd (niet geïmporteerd als module)
-if __name__ == "__main__":
-    # Roept de main-functie aan om het programma uit te voeren
-    main()
+print(lijstjes)
