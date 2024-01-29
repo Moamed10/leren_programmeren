130854a4bd247d0dd80995791f3afa61bd1c41c0
Mohamed10 <99076774@mydavinci.nl>
2024-01-29 14:45:47 +0100

    a

---
+++ module_2/deel_2/deel22/vertaling.py
@@ -0,0 +1,30 @@
+vertaal_dict = {
+    "hart": "ingang",
+    "grot": "geit",
+    "Draganthor": "Drakanthor",
+    "schubben": "teennagels",
+    "vurige": "waterende"
+}
+
+input_tekst = input("Voer een stukje tekst in: ")
+
+mo = 0
+for key,waarde in vertaal_dict.items():
+    if key in input_tekst:
+     input_tekst = input_tekst.replace(key,waarde)
+
+
+print(input_tekst)
+
+    
+
+
+        
+  
+
+
+
+
+
+
+# print(input_tekst)
